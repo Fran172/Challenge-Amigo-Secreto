865283563e4439cfce747d8546fe3cb93a7eb8da
Francisco Estevez <franciscoestevez987@gmail.com>
2025-02-09 00:04:35 -0300

    crea funcion ActualizarListaAmigos

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 let amigos = [];
 
+function ActualizarListaAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i=0; i<amigos.length; i++) {
+        let item = document.createElement("li");
+        item.innerHTML = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
 function agregarAmigo() {
-    const elemento = document.getElementById("amigo");
-    let nombre = elemento.value;
+    let cuadroDeTexto = document.getElementById("amigo");
+    let nombre = cuadroDeTexto.value;
     if (nombre === "") {
         alert("Por favor, ingrese un nombre.");
     } else {
         amigos.push(nombre);
-        elemento.value = "";
+        cuadroDeTexto.value = "";
     }
-    elemento.focus();
+    ActualizarListaAmigos();
+    cuadroDeTexto.focus();
     // console.log(amigos);
 }
