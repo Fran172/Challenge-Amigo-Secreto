87fff09079e15fccbc151ee2ca6b91755c3faaab
Francisco Estevez <franciscoestevez987@gmail.com>
2025-02-09 22:53:18 -0300

    crea funciones sortearAmigo y reiniciarSorteo

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-let amigos = [];
+let listaAmigos = [];
+let listaAmigosSorteados = [];
 
 function ActualizarListaAmigos() {
     const lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
-    for (let i=0; i<amigos.length; i++) {
+    for (let i=0; i<listaAmigos.length; i++) {
         let item = document.createElement("li");
-        item.innerHTML = amigos[i];
+        item.innerHTML = listaAmigos[i];
         lista.appendChild(item);
     }
 }
 
+function reiniciarSorteo() {
+    listaAmigos = [];
+    listaAmigosSorteados = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+}
+
 function agregarAmigo() {
-    let cuadroDeTexto = document.getElementById("amigo");
-    let nombre = cuadroDeTexto.value;
+    let inputAmigo = document.getElementById("amigo");
+    let nombre = inputAmigo.value;
     if (nombre === "") {
         alert("Por favor, ingrese un nombre.");
     } else {
-        amigos.push(nombre);
-        cuadroDeTexto.value = "";
+        listaAmigos.push(nombre);
+        inputAmigo.value = "";
     }
     ActualizarListaAmigos();
-    cuadroDeTexto.focus();
-    // console.log(amigos);
+    inputAmigo.focus();
+}
+
+function sortearAmigo() {
+    let amigo = null;
+    if (listaAmigos.length===0) {
+        alert("Por favor, ingrese un nombre.")
+    } else if (listaAmigosSorteados.length == listaAmigos.length) {
+        alert("Todos tus amigos fueron sorteados!");
+        reiniciarSorteo();
+    } else {
+        do {
+            amigo = listaAmigos[Math.floor(Math.random()*listaAmigos.length)];
+        } while (listaAmigosSorteados.includes(amigo));
+
+        listaAmigosSorteados.push(amigo);
+        document.getElementById("resultado").innerHTML = amigo;
+    }
 }
