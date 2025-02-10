eb54f263f1d4b4b8315ba89cf46fa8046ddc8f8f
Francisco Estevez <franciscoestevez987@gmail.com>
2025-02-09 23:15:56 -0300

    documenta codigo

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ let listaAmigos = [];
 let listaAmigosSorteados = [];
 
 function ActualizarListaAmigos() {
+    // borra el contenido de la lista <ul>
     const lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
+    // agrega cada nombre a la lista <ul>
     for (let i=0; i<listaAmigos.length; i++) {
         let item = document.createElement("li");
         item.innerHTML = listaAmigos[i];
@@ -19,11 +21,14 @@ function reiniciarSorteo() {
 }
 
 function agregarAmigo() {
+    // Obtiene nombre ingresado
     let inputAmigo = document.getElementById("amigo");
     let nombre = inputAmigo.value;
+    // si no hay nombre, alerta
     if (nombre === "") {
         alert("Por favor, ingrese un nombre.");
     } else {
+        // si hay nombre, lo guarda
         listaAmigos.push(nombre);
         inputAmigo.value = "";
     }
@@ -33,16 +38,19 @@ function agregarAmigo() {
 
 function sortearAmigo() {
     let amigo = null;
+    // si no hay nombres, alerta
     if (listaAmigos.length===0) {
         alert("Por favor, ingrese un nombre.")
+        // si todos fueron sorteados, reinicia
     } else if (listaAmigosSorteados.length == listaAmigos.length) {
         alert("Todos tus amigos fueron sorteados!");
         reiniciarSorteo();
     } else {
+        // si hay nombre/s sin sortear, busca hasta encontrar
         do {
             amigo = listaAmigos[Math.floor(Math.random()*listaAmigos.length)];
         } while (listaAmigosSorteados.includes(amigo));
-
+        // guarda e imprime
         listaAmigosSorteados.push(amigo);
         document.getElementById("resultado").innerHTML = amigo;
     }
